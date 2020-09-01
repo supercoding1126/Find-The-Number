7b463f747b03044cf66f8d4c2f9de4998f9de4a9
prothepro1234 <66332641+prothepro1234@users.noreply.github.com>
2020-09-01 19:05:48 -0400

    first commit

---
+++ Guess The Number.py
@@ -0,0 +1,34 @@
+import random
+
+
+number = random.randint(0, 101)
+guesses = 0
+
+
+your_guess = int(input('Guess a number between 0 and 101 press [Enter] to exit : '))
+guesses += 1
+
+while your_guess != '':
+   while your_guess != number:
+      if your_guess > number:
+         print('Your guess: ',your_guess,' is to high. ')
+         guesses += 1
+      if your_guess <= number:
+         print('Your guess: ',your_guess,'is to low. ')
+         guesses += 1
+
+      your_guess = int(input('Guess again:'))
+
+
+
+   print('You got it the number was', number, '!! You did: ',guesses,' guesses.')
+   
+   if guesses < bestscore:
+      biggestscore = guesses
+
+
+   your_guess = int(input('Guess a number between 0 and 101 press [Enter] to exit: '))
+   
+   number = random.randint(0, 101)
+
+   guesses = 0
